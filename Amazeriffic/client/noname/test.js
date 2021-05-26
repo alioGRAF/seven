d24a1ac8740333ff67eec0acbcb298e9d465ebb3
Gafurova Aliya <GafurovaAlR@stud.kai.ru>
2021-05-26 13:05:50 +0300

    start

---
+++ Amazeriffic/client/noname/test.js
@@ -0,0 +1,22 @@
+var toDoObjects = [
+{
+"description" : "Сделать покупки",
+"tags" : [ "шопинг", "рутина" ]
+},
+{
+"description" : "Сделать несколько новых задач",
+"tags" : [ "писательство", "работа" ]
+},
+/* etc */
+];
+var organizeByTags = function (toDoObjects) {
+console.log("organizeByTags вызвана");
+};
+var main = function () {
+"use strict";
+var organizeByTags = function () {
+console.log("organizeByTags вызвана");
+};
+organizeByTags(toDoObjects);
+};
+$(document).ready(main);
